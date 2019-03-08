a6310bcfc5971c87159bcf9f969638546650c82b
Wesley Fukui <31214166+Wesf31@users.noreply.github.com>
2019-03-07 20:46:46 -0700

    Car

---
+++ Car.ts
@@ -0,0 +1,13 @@
+class Car {
+    engine: string;
+    constructor(engine: string) {
+        this.engine = engine;
+    }
+    start () {
+        alert('Engine started: ' + this.engine)
+    }
+
+    stop () {
+        alert('Engine stopped: ' + this.engine)
+    }
+}
