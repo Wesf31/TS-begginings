b2a26c8c49dbc70cfd3520fe7bc4d78793baeda6
Wesley Fukui <wes.fukui@gmail.com>
2019-03-24 22:37:36 -0600

    think I got my chellenge pretty good

--- scripts/challenge.ts
+++ scripts/challenge.ts
@@ -1,5 +1,12 @@
-function translateToMorse(str: string): string {
-return 'string'
+function translateToMorse(morseCode: string): string {
+    var morseConverted = []
+    morseCode.split("").map(function (code) {
+        code.split("").map(function (dot) {
+            morseConverted.push(morse[dot]);
+        });
+        morseConverted.push(" ")
+    });
+    return morseConverted.join('') 
 }
 
 function translateToEnglish(str: string): string {
@@ -8,12 +15,13 @@ function translateToEnglish(str: string): string {
         word.split(" ").map(function (letter) {
             messageConverted.push(alphabet[letter]);
         });
-        messageConverted.push(" ")
+        messageConverted.push(" ");
     });
-    return messageConverted.join('') 
+   return messageConverted.join('')
 }
 
-var str: string = '.. - .----. ... / .-- . -.. -. . ... -.. .- -.-- / -- -.-- / -.. ..- -.. . -.-.--'
+var str: string = '.. / -- .. ... ... / .--- .- .--. .- -. . ... . / .-. .- -- . -. -.-.--'
+var mCode: string = `i miss japanese ramen`
 var alphabet: object = {  
    "-----":"0",
    ".----":"1",
@@ -56,4 +64,47 @@ var alphabet: object = {
    "·-·-·-":".",
    "--··--":","
 };
+var morse: object = {  
+    "0":"-----",
+    "1" :".----",
+    "2":"..---",
+    "3":"...--",
+    "4":"....-",
+    "5":".....",
+    "6":"-....",
+    "7":"--...",
+    "8":"---..",
+    "9":"----.",
+    "a":".-",
+    "b":"-...",
+    "c":"-.-.",
+    "d":"-..",
+    "e":".",
+    "f":"..-.",
+    "g":"--.",
+    "h":"....",
+    "i":"..",
+    "j":".---",
+    "k":"-.-",
+    "l":".-..",
+    "m":"--",
+    "n":"-.",
+    "o":"---",
+    "p":".--.",
+    "q":"--.-",
+    "r":".-.",
+    "s":"...",
+    "t":"-",
+    "u":"..-",
+    "v":"...-",
+    "w":".--",
+    "x":"-..-",
+    "y":"-.--",
+    "z":"--..",
+    " ":"/",
+    "!":"-·-·--",
+    ".":"·-·-·-",
+    ",":"--··--"
+ };
 console.log(translateToEnglish(str))
+console.log(translateToMorse(mCode))
