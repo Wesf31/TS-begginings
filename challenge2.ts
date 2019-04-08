0ac5409be3f61c5463cac13a2101614787d27bfb
Wesley Fukui <wesley.fukui@p86542.local>
2019-04-08 16:25:02 -0600

    .find messin around

--- challenge2.ts
+++ challenge2.ts
@@ -1,12 +1,12 @@
 function questionsMarks(str: string) : string {
-    console.log(str)
-    return 'true'
+    // console.log(str.split('').find(x => x == '?'))
+ return str.split('').find(x => x == '?')
 }
 
 let testData: string = 'aa6?9'
 let testData1: string = 'acc?7??sss?3rr1?????????5'
 
-questionsMarks(testData)
+questionsMarks(testData) 
 // Have the function QuestionsMarks(str) take the str string parameter, which will contain single 
 // digit numbers, letters, and question marks, and check if there are exactly 3 question marks
 // between every pair of two numbers that add up to 10. If so, then your program should return the 
