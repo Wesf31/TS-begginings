cd55dba6c24f9257c5a3df6ff1834eab0a3893b3
Wesley Fukui <wesley.fukui@p86542.local>
2019-05-14 10:00:28 -0600

    trying to get this crap worked out

--- challenge4.ts
+++ challenge4.ts
@@ -1,36 +1,34 @@
 interface IUnit {
-    name: string;
+    name?: string;
     daysActive: number;
     move(city: string): void;
-    // activityCounter(daysActive: string): void
 }
 
 abstract class Commands {
-    activityCounter(enemy: IUnit, friendylySoldier: IUnit): void {
+    public activityCounter(enemy: IUnit, friendylySoldier: IUnit): void {
         enemy.daysActive++, friendylySoldier.daysActive++
     }
     attack<T extends IUnit >(enemy: T ): void {
+        this.activityCounter(enemy, friendylySoldier)
         console.log(`Attacking enemy unit ${enemy.name} sir!` )
     }
     defend(city: string) : void {
-        
+        this.activityCounter(enemySoldier, friendylySoldier)       
         console.log(`Defending city of ${city}`);
+    }
+    move(city: string) : void {
+        this.activityCounter(enemySoldier, friendylySoldier)
+        console.log(`Moving to city of ${city}`);
     }
 }
 
 interface IMedic extends IUnit {
     heal(soldier: IUnit): void;
-    //has name daysactive and move
 }
-// extract the attack method out of both interfaces and make them generic
 
-interface ITank {
+interface ITank extends IUnit, Commands {
     callSign: string;
-    daysActive: number;
     crewNumber: number;
-    attack(enemy: ITank): void;
-    defend(city: string): void;
-    move(city: string): void;
 }
 
 abstract class Submarine {
@@ -43,25 +41,19 @@ abstract class Submarine {
     }
 }
 
-//Because I am using the attack, defend, move methods in more than 1 class I thought I would make an abstract class
-// with just those actions, see below, and just use inheretience with on the concreate classes 
-
-class Soldier extends Action implements ISoldier {
+class Soldier extends Commands implements IUnit {
     constructor(public name: string) {
-        super();
+        super()
     }
-    attack(enemy: ISoldier): void  {
-        this.daysActive++;
-        enemy.daysActive++;
-        // still telling not asking as far as incrementing the days active, exposed method on the enemy and friendly soldier that ask to increment their daysActive
-        console.log(`Attacking enemy soldier ${enemy.name} sir!`);
-    }
+    daysActive : number;
 }
 
-class Medic extends Soldier implements IMedic {
-    heal(soldier: ISoldier) : void {
-        soldier.daysActive++;
-        this.daysActive++;
+class Medic extends Commands implements IMedic {
+    constructor(public name: string) {
+        super()
+    }
+    public daysActive : number;
+    heal(soldier: IUnit) : void {
         console.log(`Healing solider ${soldier.name}`);
     }
 }
@@ -71,7 +63,6 @@ class SubmarineConcreate extends Submarine {
         super ();
     }
     move(city: string) : void {
-        this.daysActive++;
         console.log(`Moving to city of ${city}`);
     }
     //def need to use move method across all the different concreate classes
@@ -81,16 +72,11 @@ class Tank extends Commands implements ITank  {
     constructor ( public callSign: string) {
         super();
     }
-    public crewNumber: number;
-    attack(enemy: ITank): void  {
-
-        enemy.daysActive++;
-        console.log(`Attacking enemy ${enemy.callSign} sir!`);
-    }
+    daysActive: 0;
+    crewNumber: number;
 }
 
 const friendylySoldier = new Soldier ("Mark");
-
 const enemySoldier = new Soldier("Jeff");
 
 friendylySoldier.attack(enemySoldier);
